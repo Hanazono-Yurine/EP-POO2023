555b8d7d073aa09e5d4be7fb92b7da443c106daa
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2023-09-19 18:19:45 -0300

    Implementation of the class Somador

--- src/Somador.cpp
+++ src/Somador.cpp
@@ -8,4 +8,14 @@ Somador::~Somador() {
 }
 
 Sinal* Somador::processar(Sinal* sinalIN, Sinal* sinalIN2) {
+	const double *sequenciaClone = sinalIN->getSequencia(); 
+	const double *sequencia2Clone = sinalIN2->getSequencia(); 
+	const int maxLengthOut = (sinalIN->getComprimento() <= sinalIN2->getComprimento()) ? sinalIN->getComprimento() : sinalIN2->getComprimento();
+	double *sinalProc = new double[maxLengthOut];
+	for (int index = 0; index < maxLengthOut; index++) {
+		sinalProc[index] = sequenciaClone[index] + sequencia2Clone[index];
+	}
+	Sinal *sinalOUT = new Sinal(sinalProc, maxLengthOut);
+	delete[] sinalProc;
+	return sinalOUT;
 }
