f5b9c755b7f77cd2e9fa26dc90b608add4789291
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2023-09-19 16:03:21 -0300

    Implementation of the class Integrador

--- src/Integrador.cpp
+++ src/Integrador.cpp
@@ -2,10 +2,21 @@
 #include "Sinal.h"
 
 Integrador::Integrador() {
+
 }
 
 Integrador::~Integrador() {
+
 }
 
 Sinal* Integrador::processar(Sinal* sinalIN) {
+	const double *sequenciaClone = sinalIN->getSequencia(); 
+	double *sinalProc = new double[sinalIN->getComprimento()];
+	sinalProc[0] = sequenciaClone[0];
+	for (int index = 1; index < sinalIN->getComprimento(); index++) {
+		sinalProc[index] = sinalProc[index - 1] + sequenciaClone[index];
+	}
+	Sinal *sinalOUT = new Sinal(sinalProc, sinalIN->getComprimento());
+	delete[] sinalProc;
+	return sinalOUT;
 }
