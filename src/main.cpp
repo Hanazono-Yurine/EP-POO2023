087fa2c2bb04cfbd001d6a2d0b85801ad32cb390
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2023-09-19 20:56:45 -0300

    Modified main file for debbuging

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
+#include "ModuloRealimentado.h"
+#include "Sinal.h"
+#include "Amplificador.h"
+#include "Integrador.h"
+
 void menu();
+void test();
 
 int main() {
 	menu();
+	test();
 	return 0;
+}
+
+void menu() {
+
+}
+
+void test() {
+	ModuloRealimentado test = ModuloRealimentado(0.3);
+	double justatest[20] = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+	Sinal *finalTest = test.processar(new Sinal(justatest, 20));
+	finalTest->imprimir("test");
 }
