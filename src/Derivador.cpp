4141dd5b01a5959f3922809b5bb076b435f9290f
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2023-09-19 16:03:57 -0300

    Implementation of the class Derivador

--- src/Derivador.cpp
+++ src/Derivador.cpp
@@ -1,10 +1,21 @@
 #include "Derivador.h"
 
 Derivador::Derivador() {
+
 }
 
 Derivador::~Derivador() {
+
 }
 
 Sinal* Derivador::processar(Sinal* sinalIN) {
+	const double *sequenciaClone = sinalIN->getSequencia(); 
+	double *sinalProc = new double[sinalIN->getComprimento()];
+	sinalProc[0] = sequenciaClone[0];
+	for (int index = 1; index < sinalIN->getComprimento(); index++) {
+		sinalProc[index] = sequenciaClone[index] - sequenciaClone[index - 1];
+	}
+	Sinal *sinalOUT = new Sinal(sinalProc, sinalIN->getComprimento());
+	delete[] sinalProc;
+	return sinalOUT;
 }
