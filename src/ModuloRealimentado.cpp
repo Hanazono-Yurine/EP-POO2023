9fee1a44d37ec00d0a27e419de022fd12929a0d5
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2023-10-02 14:28:11 -0300

    Add some coments
    Because the other collaborator is a baby that cant can read

--- src/ModuloRealimentado.cpp
+++ src/ModuloRealimentado.cpp
@@ -16,20 +16,31 @@ ModuloRealimentado::~ModuloRealimentado() {
 }
 
 Sinal* ModuloRealimentado::processar(Sinal* sinalIN) {
+	// Cria um inversor, um piloto e um somador
 	Amplificador inverter = Amplificador(-1.0);
 	Piloto piloto = Piloto(this->ganho);
 	Somador sum = Somador();
 
+	// Cria um array vazio com o tamanho do objeto de entrada
 	double *diff = new double[sinalIN->getComprimento()];
 
+	// Cria um objeto Sinal com a sequencia vazia
 	Sinal *sinalOUT = new Sinal(diff, sinalIN->getComprimento());
+	// Cria um objeto Sinal com a sequencia do objeto de entrada
 	Sinal sinalDiff = Sinal(sinalIN->getSequencia(), sinalIN->getComprimento());
+	// Cria um objeto Sinal com a sequencia vazia
 	Sinal sinalInverted = Sinal(diff, sinalIN->getComprimento());
 
+	// Deleta o array vazio
+	delete[] diff;
+
+	// Processa o primeiro valor da sequencia
 	sinalOUT->getSequencia()[0] = piloto.processar(sinalIN)->getSequencia()[0];
+	// Copia o valor processado pro primeiro elemento
 	sinalDiff.getSequencia()[0] = sinalIN->getSequencia()[0];
 
 	for (int index = 1; index < sinalIN->getComprimento(); index++) {
+		// Daqui em diante é só lê no pdf
 		sinalInverted.getSequencia()[index] = inverter.processar(sinalOUT)->getSequencia()[index - 1];
 
 		sinalDiff.getSequencia()[index] = sum.processar(&sinalDiff, &sinalInverted)->getSequencia()[index];
