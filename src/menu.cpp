517e9778ad58e6f7925fb68f6a31d84d475d8173
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2023-10-03 11:42:36 -0300

    feat: Add the user prompt

--- src/menu.cpp
+++ src/menu.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include "ModuloRealimentado.h"
 #include "Sinal.h"
+#include "Somador.h"
 #include "Amplificador.h"
 #include "Integrador.h"
+#include "Derivador.h"
 
 #define MAX_STEPS 60
 
@@ -13,54 +15,56 @@ Sinal* newSinal() {
 
 	double arraySinal[MAX_STEPS];
 
-	cout << "Qual sinal voce gostaria de utilizar como entrada da sua simulacao?\n1) 5 + 3 * cos(n * pi / 8)\n2) constante\n3) rampa\nEscolha : ";
+	cout << "\nQual sinal voce gostaria de utilizar como entrada da sua simulacao?" << endl;
+	cout << "1) 5 + 3 * cos(n * pi / 8)\n2) constante\n3) rampa\nEscolha : ";
+
 	int choiceSinal = 0;
 	cin >> choiceSinal;
-	if (choiceSinal == 1) {
 
-		for (int i = 0; i < MAX_STEPS; i++) {
-			arraySinal[i] = 5 + 3 * cos(i * M_PI / 8);
-		}
+	switch (choiceSinal) {
+		case 1:
+			for (int i = 0; i < MAX_STEPS; i++) {
+				arraySinal[i] = 5 + 3 * cos(i * M_PI / 8);
+			}
+			break;
+		case 2:
+			{
+				cout << "\nQual o valor dessa constante?\nC = ";
 
-		return new Sinal(arraySinal, MAX_STEPS);
-	}
-	else if (choiceSinal == 2) {
-		cout << "Qual o valor dessa constante?\nC = ";
+				double constValue = 0;
+				cin >> constValue;
 
-		double constValue = 0;
-		cin >> constValue;
+				for (int i = 0; i < MAX_STEPS; i++) {
+					arraySinal[i] = constValue;
+				}
+			}
+			break;
+		case 3:
+			cout << "\nQual a inclinacao dessa rampa?\na = ";
 
-		for (int i = 0; i < MAX_STEPS; i++) {
-			arraySinal[i] = constValue;
-		}
+			double slope = 0;
+			cin >> slope;
 
-		return new Sinal(arraySinal, MAX_STEPS);
-	}
-	//supondo que o usuario so digita 1, 2 e 3
-
-	cout << "Qual a inclinacao dessa rampa?\na = ";
-
-	double slope = 0;
-	cin >> slope;
-
-	for (int i = 0; i < MAX_STEPS; i++) {
-		arraySinal[i] = slope*i;
+			for (int i = 0; i < MAX_STEPS; i++) {
+				arraySinal[i] = slope*i;
+			}
+			break;
 	}
 
 	return new Sinal(arraySinal, MAX_STEPS);
-
 }
 
 void menu() {
 
-	double cosineWave[60];
+	double cosineWave[MAX_STEPS];
 
-	for (int i = 0; i < 60; i++) {
+	for (int i = 0; i < MAX_STEPS; i++) {
 		cosineWave[i] = 5 + 3 * cos(i*M_PI/8);
 		//cout << cosineWave[i] << ", ";
 	}
 
-	cout << "Simulink em C++\nQual simulacao voce gostaria de fazer ?\n1) Piloto Automatico\n2) Sua propria sequencia de operacoes\nEscolha : ";
+	cout << "        Simulink em C++" << endl;
+	cout << "Qual simulacao voce gostaria de fazer ?\n1) Piloto Automatico\n2) Sua propria sequencia de operacoes\nEscolha : ";
 
 	int choice = 0;
 	cin >> choice;
@@ -69,16 +73,89 @@ void menu() {
 
 	if (choice == 1) {
 
-		cout << "Qual o ganho do acelerador?\ng = ";
+		cout << "\nQual o ganho do acelerador?\ng = ";
 
 		double acceleration = 0;
 		cin >> acceleration;
 
+		cout << endl;
+
 		ModuloRealimentado *moduloRealimentado = new ModuloRealimentado(acceleration);
-		Sinal* processedSignal = moduloRealimentado->processar(signalSim);
+		moduloRealimentado->processar(signalSim)->imprimir("Velocidade do Carro");
 
-		processedSignal->imprimir("Velocidade do Carro");
+	} else if (choice == 2) {
+		bool repeat = true;
 
+		while (repeat) {
+			repeat = false;
+
+			cout << "\nQual operacao voce gostaria de fazer?" << endl;
+			cout << "1) Amplificar\n2) Somar\n3) Derivar\n4) Integrar" << endl;
+			cout << "Escolha : ";
+
+			int operationChoice = 0;
+			cin >> operationChoice;
+
+			switch (operationChoice) {
+				case 1:
+					{
+						cout << "\nQual o ganho dessa amplificação?\ng = ";
+
+						double userAmp = 0.0;
+						cin >> userAmp;
+
+						Amplificador amp = Amplificador(userAmp);
+						Sinal *tempSignal = amp.processar(signalSim);
+						delete signalSim;
+						signalSim = tempSignal;
+					}
+					break;
+				case 2:
+					{
+						cout << "\nInforme mais um sinal para ser somado.";
+
+						Sinal *signalToSum = newSinal();
+
+						Somador sum = Somador();
+						Sinal *tempSignal = sum.processar(signalSim, signalToSum);
+						delete signalToSum;
+						delete signalSim;
+						signalSim = tempSignal;
+					}
+					break;
+				case 3:
+					{
+						Derivador deriv = Derivador();
+						Sinal *tempSignal = deriv.processar(signalSim);
+						delete signalSim;
+						signalSim = tempSignal;
+					}
+					break;
+				case 4:
+					{
+						Integrador integ = Integrador();
+						Sinal *tempSignal = integ.processar(signalSim);
+						delete signalSim;
+						signalSim = tempSignal;
+					}
+					break;
+			}
+
+			cout << "\nO que voce quer fazer agora?" << endl;
+			cout << "1) Realizar mais uma operacao no resultado\n2) Imprimir o resultado para terminar" << endl;
+			cout << "Escolha : ";
+
+			int repeatChoice = 0;
+			cin >> repeatChoice;
+
+			if (repeatChoice == 1)
+				repeat = true;
+		}
+
+		cout << endl;
+
+		signalSim->imprimir("Resultado Final");
+		delete signalSim;
 	}
 
 }
