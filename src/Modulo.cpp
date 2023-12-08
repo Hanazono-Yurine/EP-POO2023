f5fed0a431f9d96c24923e59a61100f50afc1a04
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2023-12-08 02:49:56 -0300

    Add Modulo class

---
+++ src/Modulo.cpp
@@ -0,0 +1,30 @@
+#include <list>
+#include <iostream>
+#include "CircuitoSISO.h"
+#include "Modulo.h"
+
+Modulo::Modulo() : circuitList(new std::list<CircuitoSISO *>){
+
+}
+	
+Modulo::~Modulo() {
+	delete this->circuitList;
+}
+	
+void Modulo::adicionar(CircuitoSISO *circ) {
+	this->circuitList->push_back(circ);
+}
+	
+std::list<CircuitoSISO*> *Modulo::getCircuitos() {
+	return this->circuitList;
+}
+
+void Modulo::imprimir() {
+	std::cout << "Modulo com ID " << this->getID() << " e: " << std::endl;
+
+	std::list<CircuitoSISO *>::iterator it = this->circuitList->begin();
+	while (it != this->circuitList->end()) {
+		(*it)->imprimir();
+	}
+	std::cout << "--" << std::endl;
+}
---
+++ src/Modulo.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CircuitoSISO.h"
+#include <list>
+
+class Modulo : public CircuitoSISO {
+	public:
+		Modulo();
+		virtual ~Modulo();
+		virtual void adicionar(CircuitoSISO *circ);
+		std::list <CircuitoSISO*> *getCircuitos();
+		void imprimir();
+	private:
+		std::list<CircuitoSISO *> *circuitList;
+};
